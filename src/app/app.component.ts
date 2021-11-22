e810556514d00082f209acd62e2e5665e80b26f3
Panchaya-M <panchaya.meeboonpor@gmail.com>
2021-11-22 20:39:51 +0700

    Add leap years

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leap-year';
+  year: number = 0
+  test: boolean = true
+
+  leapYear() {
+    if (this.year % 400 == 0) { this.test = true }
+    else if (this.year % 400 != 0 && this.year % 100 !=0) {
+      if (this.year % 4 == 0) { this.test = true }
+      else { this.test = false }
+    }
+  }
 }
